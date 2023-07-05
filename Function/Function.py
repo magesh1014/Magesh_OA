e53525f11f1eb6d59a8a8be55a7e88319d220ac4
magesh1014 <magesht1014@gmail.com>
2023-07-05 17:19:55 +0530

    done

---
+++ Function/Function.py
@@ -0,0 +1,25 @@
+#  a function is a set of instruction to perform a specific task
+
+# python functions :
+
+def multiply():
+# def = keyword that means "define"
+# multiply = function name
+    a = 2       # function body
+    b = 3       # a and b were created inside the function
+    return a*b
+# return represent end of the function 
+# [a*b] = must output some kind of data type {432,false,"string",[1,2,3]}
+# in absence of data, return statement can be skip by replacing print statement
+    
+    # print(hello)
+
+# only defining the function is not enough, we also need to call it
+multiply()      #call function
+
+if a>b:         # a and b don't exist outside the function
+    print(a)
+
+# # # NameError : name "a" is not defined
+
+ 
